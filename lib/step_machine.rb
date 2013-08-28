7832b48a093782876e5bbbe4a286fc22eb904d32
Rafael Vettori <rafael.vettori@gmail.com>
2013-08-28 07:28:48 -0300

    Adjust when walk return nil value; Created method for clean_queues

--- lib/step_machine.rb
+++ lib/step_machine.rb
@@ -5,55 +5,72 @@ require "step_machine/step"
 
 module StepMachine
 
-  # Fila para execução
-  def queue
-    @queue ||= []
+  module ClassMethods
   end
 
-  # Fila das execuções completadas
-  def queue_completed
-    @queue_completed ||= []
-  end
+  module InstanceMethods
 
-  # Fila das execuções com falhadas
-  def queue_failed
-    @queue_failed ||= []
-  end
-
-  def add_step(name, param=nil, block)
-    step = Step.new
-    step.name = name
-    step.param = param
-    step.block = block.instance_of?(String) ? eval(block) : block
-    raise ArgumentError, "invalid block parameter of method add_step(#{name}, #{param}, #{block})" unless step.block.instance_of?(Proc)
-    queue << step
-  end
-
-  def step(name, param=nil, &block)
-    add_step(name, param, block)
-  end
-
-  # Execute one step of queue
-  def walk
-    step = queue.delete(queue.first)
-    begin
-      step.result = step.block.call(step.param)
-      queue_completed << step
-    rescue Exception => e
-      step.result = e.message
-      step.error = true
-      queue_failed << step
+    def clear_queues
+      @queue = []
+      @queue_failed = []
+      @queue_completed = []
     end
-    step
-  end
 
-  # Execute all steps of queue until first error
-  def walking
-    while queue.first
-      step = walk
-      yield(step) if block_given?
-      break if step.error?
+    # Fila para execução
+    def queue
+      @queue ||= []
     end
+
+    # Fila das execuções completadas
+    def queue_completed
+      @queue_completed ||= []
+    end
+
+    # Fila das execuções com falhadas
+    def queue_failed
+      @queue_failed ||= []
+    end
+
+    def add_step(name, param=nil, block)
+      step = Step.new
+      step.name = name
+      step.param = param
+      step.block = block.instance_of?(String) ? eval(block) : block
+      raise ArgumentError, "invalid block parameter of method add_step(#{name}, #{param}, #{block})" unless step.block.instance_of?(Proc)
+      queue << step
+    end
+
+    def step(name, param=nil, &block)
+      add_step(name, param, block)
+    end
+
+    # Execute one step of queue
+    def walk
+      step = queue.delete(queue.first) || (return nil)      
+      begin
+        step.result = step.block.call(step.param)
+        queue_completed << step
+      rescue Exception => e
+        step.result = e.message
+        step.error = true
+        queue_failed << step
+      end
+      step
+    end
+
+    # Execute all steps of queue until first error
+    def walking
+      while queue.count > 0
+        step = walk
+        yield(step) if block_given?
+        break if (step.nil? || step.error?)
+      end
+    end
+
   end
 
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
 end
--- spec/step_machine_spec.rb
+++ spec/step_machine_spec.rb
@@ -2,124 +2,159 @@ require "spec_helper"
 
 include StepMachine
 
-describe "On step machine" do
+describe "StepMachine" do
 
 	before :each do
 	end
 
-	it "should create an instance of step" do
-		step = Step.new
-		step.should be_an_instance_of StepMachine::Step
-	end
+	describe "when enqueue" do
 
-	it "should add step with block do..end and {}" do
-	  step('name', 'value param') do |param|
-	  	param
-	  end
-	  step('name', 'value param') { |param| param }
-	  queue.count.should == 2
-	end
-
-	it "should add step with block and validate block param" do
-	  expect { add_step_by_block('name', 'param')}.to raise_error
-	end
-
-	it "should add step to queue" do
-		add_step("Step 1", "params 1", proc {"block"})
-		add_step("Step 2", "params 2", proc {"block"})
-		add_step("Step 3", proc {"block"})
-		queue.count.should equal(3)
-	end
-
-	it "should validate object proc of block attribute" do
-		add_step("Step 1", "params 1", proc {"block"})
-		queue[0].block.should be_an_instance_of Proc
-		expect{ add_step("Step 1", "params 1", "non proc") }.to raise_error
-		queue.count.should equal 1
-	end
-
-	it "should validate object proc of block attribute when passed proc in string format" do
-		add_step("Step 1", "params 1", %{proc {"block"}})
-		queue.count.should equal 1
-		expect { add_step("Step 1", "params 1", %{non block string}) }.to raise_error
-		queue.count.should equal 1
-	end
-
-	it "should add in queue_failed if failed execution" do
-		add_step("Step 1", "params 1", proc { raise "Forced Error" })
-		walk.error?.should be_true
-		queue_failed.count.should equal 1
-	end
-
-	it "should excecute the next step of queue" do
-		add_step("Step 1", "params 1", proc {"block"})
-		queue.count.should equal 1
-		walk
-		queue.count.should equal 0
-	end
-
-	it "should add in queue_completed if correct execution" do
-		add_step("Step 1", "params 1", proc { "block" })
-		walk
-		queue_completed.count.should equal 1
-	end
-
-	it "the queues should be empty to initialize" do
-		queue.should_not be nil
-		queue_completed.should_not be nil
-	end
-
-	it "should execute all steps of queue" do
-		add_step("Step 1", "params 1", proc { "block" })
-		add_step("Step 2", "params 2", proc { "block" })
-		add_step("Step 3", "params 3", proc { "block" })
-		walking
-		queue.count.should equal 0
-	end
-
-	it "should execute one step of queu" do
-		add_step("Step 1", "params 1", proc { "block 1" })
-		add_step("Step 2", "params 2", proc { "block 2" })
-		walk
-		queue.count.should == 1
-		queue_completed.count.should == 1
-	end
-
-	it "should execute all steps of queue until first error" do
-		add_step("Step 1", "params 1", proc { "block" })
-		add_step("Step 2", "params 2", proc { raise "first error" })
-		add_step("Step 3", "params 3", proc { "block" })
-		walking
-		queue.count.should equal 1
-		queue_completed.count.should equal 1
-		queue_failed.count.should equal 1
-	end
-
-	it "should execute next step and return de current step" do
-		add_step("Step 1", proc { "block 1" })
-		walk.should be_an_instance_of Step
-	end
-
-	it "should execute all steps of queue until first error, using block and return current step" do
-		add_step("Step in queue_completed", proc { "block 1" })
-		add_step("Step in queue_failed", proc { raise "force exception" })
-		add_step("Step in queue", proc { "block 3" })
-		walking do |step|
-			step.should be_an_instance_of Step
-			step.error?.should be_true if step.name == "Step in queue_failed"
-			step.error?.should be_false if step.name != "Step in queue_failed"
+		it "should create an instance of step" do
+			step = Step.new
+			step.should be_an_instance_of StepMachine::Step
 		end
-		queue.count.should equal 1
-		queue_completed.count.should equal 1
-		queue_failed.count.should equal 1
-	end
 
-	it "should execute all steps and field value must be equal field result " do
-		add_step("Step in queue_completed", "value_result" , proc { "value_result" })
-		add_step("Step in queue_completed", "block 1", proc { "block 1" })
-		walking do |step|
-			step.result.should == step.param
+		it "should add step with block do..end and {}" do
+			step('name', 'value param') do |param|
+				param
+			end
+			step('name', 'value param') { |param| param }
+			queue.count.should == 2
+		end
+
+		it "should add step with block and validate block param" do
+			expect { add_step_by_block('name', 'param')}.to raise_error
+		end
+
+		it "should add step to queue" do
+			add_step("Step 1", "params 1", proc {"block"})
+			add_step("Step 2", "params 2", proc {"block"})
+			add_step("Step 3", proc {"block"})
+			queue.count.should equal(3)
+		end
+
+		it "should validate object proc of block attribute" do
+			add_step("Step 1", "params 1", proc {"block"})
+			queue[0].block.should be_an_instance_of Proc
+			expect{ add_step("Step 1", "params 1", "non proc") }.to raise_error
+			queue.count.should equal 1
+		end
+
+		it "should validate object proc of block attribute when passed proc in string format" do
+			add_step("Step 1", "params 1", %{proc {"block"}})
+			queue.count.should equal 1
+			expect { add_step("Step 1", "params 1", %{non block string}) }.to raise_error
+			queue.count.should equal 1
+		end
+
+		it "should add in queue_failed if failed execution" do
+			add_step("Step 1", "params 1", proc { raise "Forced Error" })
+			walk.error?.should be_true
+			queue_failed.count.should equal 1
+		end
+
+		it "should excecute the next step of queue" do
+			add_step("Step 1", "params 1", proc {"block"})
+			queue.count.should equal 1
+			walk
+			queue.count.should equal 0
+		end
+
+		it "should add in queue_completed if correct execution" do
+			add_step("Step 1", "params 1", proc { "block" })
+			walk
+			queue_completed.count.should equal 1
+		end
+
+		it "the queues should be empty to initialize" do
+			queue.should_not be nil
+			queue_completed.should_not be nil
 		end
 	end
+
+	describe "when use queue" do
+
+		it "should execute all steps of queue" do
+			add_step("Step 1", "params 1", proc { "block" })
+			add_step("Step 2", "params 2", proc { "block" })
+			add_step("Step 3", "params 3", proc { "block" })
+			walking
+			queue.count.should equal 0
+		end
+
+		it "should execute one step of queu" do
+			add_step("Step 1", "params 1", proc { "block 1" })
+			add_step("Step 2", "params 2", proc { "block 2" })
+			walk
+			queue.count.should == 1
+			queue_completed.count.should == 1
+		end
+
+		it "should execute all steps of queue until first error" do
+			add_step("Step 1", "params 1", proc { "block" })
+			add_step("Step 2", "params 2", proc { raise "first error" })
+			add_step("Step 3", "params 3", proc { "block" })
+			walking
+			queue.count.should equal 1
+			queue_completed.count.should equal 1
+			queue_failed.count.should equal 1
+		end
+
+		it "should execute next step and return de current step" do
+			add_step("Step 1", proc { "block 1" })
+			walk.should be_an_instance_of Step
+		end
+
+		it "should execute all steps of queue until first error, using block and return current step" do
+			add_step("Step in queue_completed", proc { "block 1" })
+			add_step("Step in queue_failed", proc { raise "force exception" })
+			add_step("Step in queue", proc { "block 3" })
+			walking do |step|
+				step.should be_an_instance_of Step
+				step.error?.should be_true if step.name == "Step in queue_failed"
+				step.error?.should be_false if step.name != "Step in queue_failed"
+			end
+			queue.count.should equal 1
+			queue_completed.count.should equal 1
+			queue_failed.count.should equal 1
+		end
+
+		it "should execute all steps and field value must be equal field result " do
+			add_step("Step in queue_completed", "value_result" , proc { "value_result" })
+			add_step("Step in queue_completed", "block 1", proc { "block 1" })
+			walking do |step|
+				step.result.should == step.param
+			end
+		end
+
+		it "should stop if walking and then try walk" do
+		  step("Step 1", "value 1") { "value 1" }
+		  step("Step 2", "value 2") { "value 2" }
+		  walking
+		  queue.count.should == 0
+		  walk.should == nil
+		end
+
+		it "should stop walking if walk return nil value" do
+		  step("Step 1", "value 1") { "value 1" }
+		  queue << nil
+		  step("Step 3", "value 3") { "value 3" }
+		  queue.count.should == 3
+		  walking
+		  queue.count.should == 1
+		end
+
+		it "should clean queue, queue_completed, queue_failed if call clear_queue" do
+		  queue << 1
+		  queue_failed << 1
+		  queue_completed << 1
+		  clear_queues
+		  queue.empty?.should be_true
+		  queue_failed.empty?.should be_true
+		  queue_completed.empty?.should be_true
+		end
+	end
+
+
 
 end
