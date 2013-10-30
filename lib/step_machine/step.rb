df9a58ee0b1d24aadc3ea8c761a43f4daff574a8
Rafael Vettori <rafael.vettori@gmail.com>
2013-10-30 10:36:23 -0200

    Implemented validate and success method

--- lib/step_machine/step.rb
+++ lib/step_machine/step.rb
@@ -2,15 +2,42 @@ module StepMachine
 
   class Step
 
-    attr_accessor :name, :block, :next_step, :result
+    attr_accessor :name, :block, :next_step
+    attr_reader :exception, :result, :validation
 
     def initialize(name)
       self.name = name
     end
 
-    def perform
-      self.result = block.call
+    def validate(value = nil, &block)     
+      @validation = block || value
     end
+
+    def success(&block)
+      @success = block
+    end
+
+    def perform
+      @result = block.call
+      valid = valid?
+      @success.call(self) if @success && valid
+      valid
+    rescue => e
+      @exception = e
+      false
+    end
+
+    private
+
+    def valid?
+      if validation 
+        return validation.call(self) if validation.is_a?(Proc)
+        return result.match(validation) if validation.is_a?(Regexp)
+        return validation == result
+      end
+      true
+    end
+
    
   end
 	
--- spec/step_spec.rb
+++ spec/step_spec.rb
@@ -6,10 +6,64 @@ describe Step do
 		@step = Step.new(:name)
 		@step.block = proc { 1 + 1 }
 	end
+	
+	it "should assign a block validation to the step" do
+		block = proc {|step| step.result = 2}
 
-	it "should execute the block and store the result" do
-		@step.perform
-		@step.result.should == 2
+		@step.validate(&block)
+		@step.validation.should == block
+	end
+
+	it "should assign a value to validate the result of the step" do
+		@step.validate(1)
+		@step.validation.should == 1
+	end
+
+	describe "on perform" do
+
+		it "should execute the block and store the result" do
+			@step.perform
+			@step.result.should == 2
+		end
+
+		it "should evaluate the validation block" do
+			@step.validate {|step| step.result == 1} # invalid
+			@step.perform.should be_false 
+
+			@step.validate {|step| step.result == 2} # valid
+			@step.perform.should be_true 
+		end
+
+		it "should evaluate the validation value" do
+			@step.validate(1) # invalid
+			@step.perform.should be_false 
+
+			@step.validate(2) # valid
+			@step.perform.should be_true
+		end
+
+		it "should evaluate the validation regex" do
+			@step.block = proc { "validation" }
+			@step.validate(/^x/) # invalid
+			@step.perform.should be_false 
+
+			@step.validate(/n$/) # valid
+			@step.perform.should be_true
+		end
+
+		it "should store the exception on step when raised" do
+			@step.block = proc { raise ArgumentError }			
+			@step.perform.should be_false 
+			@step.exception.class.should == ArgumentError
+		end
+
+		it "should execute a success block" do
+			x = 0		  
+			@step.success {|step| x += 1 }
+			x.should == 0
+		  @step.perform
+			x.should == 1
+		end
 	end
 
 end
